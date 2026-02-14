538659643daa420902319c83b1e46c6020f1405d
Marek Cieślar <marek.cieslar@gmail.com>
2026-02-14 20:47:14 +0100

    fix for nginx setup

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "dart-backend",
-      script: "./server.js",
+      name: 'dart-backend',
+      script: './server.js',
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: "1G",
+      max_memory_restart: '1G',
       env_production: {
-        NODE_ENV: "production",
-        PORT: 21217,
+        NODE_ENV: 'production',
+        PORT: 3000,
       },
     },
   ],
